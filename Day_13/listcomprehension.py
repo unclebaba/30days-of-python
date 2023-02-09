c48c48d543fa4edefb7b4b07c92d90e77f161f5f
joshua baba samuel <jsachigili@gmail.com>
2023-02-09 04:37:55 -0800

    arewadc

---
+++ Day_13/listcomprehension.py
@@ -0,0 +1,3 @@
+# Filter only negative and zero in the list using list comprehension
+
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
---
+++ Day_14/higherorderfunction.py
@@ -0,0 +1,6 @@
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+##Exercises: Level 1
+# Explain the difference between map, filter, and reduce.
